7a83ff328ee15454aeaa833c68518e7ebb262ec4
Liam Cuthbert <lcu22@uclive.ac.nz>
2023-12-03 20:10:19 +1300

    Got new 360 viewer working and added control buttons to move to fullscreen

--- src/pages/DynamicPage.tsx
+++ src/pages/DynamicPage.tsx
@@ -1,18 +1,19 @@
 import React, {useMemo} from 'react';
 import { useParams } from 'react-router-dom';
-import View360, {EquirectProjection} from "@egjs/react-view360";
+import View360, {EquirectProjection, ControlBar} from "@egjs/react-view360";
 import "@egjs/react-view360/css/view360.min.css";
 
 const DynamicPage: React.FC = () => {
     const { url } = useParams<{ url: string }>();
     const projection = useMemo(() => new EquirectProjection({
-        src: `${url}.jpg`
+        src: `/${url}.jpg`,
+
     }), [])
 
     return (
         <div>
             <h2>Dynamic Page</h2>
-            <View360 className="is-16by9" projection={projection}/>
+            <View360 className="is-16by9" projection={projection} plugins={[new ControlBar()]}/>
         </div>
     );
 };
